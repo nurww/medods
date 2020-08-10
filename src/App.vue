64e1d0d48e87f8ea243a50ff1de3b74ebb0bd3d9
nurww <nurdaule@gmail.com>
2020-08-11 04:45:17 +0600

    finished //renamed component to Form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from '@/components/Form.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
